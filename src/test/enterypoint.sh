7a2478d292a998610bf2b2620caa96fae1e9bb64
Ubuntu <pashutdvir@gmail.com>
2023-01-04 17:19:35 +0000

    fix all

---
+++ src/test/enterypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python e2e_test.py tox-app:8080 sanity 5 &
+python e2e_test.py tox-app:8080 sanity 5 &
+python e2e_test.py tox-app:8080 sanity 5 &
+python e2e_test.py tox-app:8080 sanity 5 &
+python e2e_test.py tox-app:8080 sanity 5 &
+python e2e_test.py tox-app:8080 sanity 5 
+
+
