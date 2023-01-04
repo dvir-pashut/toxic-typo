1b7b691dc29d376f66fc85f10df27d8589effc51
Ubuntu <pashutdvir@gmail.com>
2023-01-04 17:35:54 +0000

    fix all

--- src/test/enterypoint.sh
+++ src/test/enterypoint.sh
@@ -2,12 +2,12 @@
 
 cat e2e | head -n 50 > e2e.1-50
 cat e2e | head -n 100 | tail -n 50 > e2e.51-100
-cat e2e | head -n 150 | tail -n 100 > e2e.101-150
-cat e2e | head -n 200 | tail -n 150 > e2e.151-200
-cat e2e | head -n 250 | tail -n 200 > e2e.201-250
-cat e2e | head -n 300 | tail -n 250 > e2e.251-300
-cat e2e | head -n 350 | tail -n 300 > e2e.301-350
-cat e2e | head -n 399 | tail -n 350 > e2e.350-399
+cat e2e | head -n 150 | tail -n 50 > e2e.101-150
+cat e2e | head -n 200 | tail -n 50 > e2e.151-200
+cat e2e | head -n 250 | tail -n 50 > e2e.201-250
+cat e2e | head -n 300 | tail -n 50 > e2e.251-300
+cat e2e | head -n 350 | tail -n 50 > e2e.301-350
+cat e2e | head -n 399 | tail -n 50 > e2e.350-399
 
 
 python e2e_test.py tox-app:8080 e2e.51-100  5 &
