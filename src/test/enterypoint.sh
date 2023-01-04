8570b7463cc2b3b639a98024c9ce56b56293d98f
Ubuntu <pashutdvir@gmail.com>
2023-01-04 20:28:28 +0000

    fix all

--- src/test/enterypoint.sh
+++ src/test/enterypoint.sh
@@ -24,6 +24,6 @@
 
 for (( i=$t; i<=$key; i+=20 )); do
     cat e2e | head -n $i | tail -n 20 > e2e.file$i
-    python e2e_test.py $app e2e.file$i 1 &
+    python e2e_test.py $app e2e.file$i  &
 done
 wait
