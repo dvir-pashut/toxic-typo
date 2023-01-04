d045b41e6f83d6aed8cec264bcfd0ef9c9ea7d0a
Ubuntu <pashutdvir@gmail.com>
2023-01-04 18:08:31 +0000

    fix all

--- src/test/enterypoint.sh
+++ src/test/enterypoint.sh
@@ -3,7 +3,7 @@
 sleep 3
 for (( i=0; i<=400; i+=25 )); do
     echo $i
-    cat e2e | head -n $i | tail -n 25 > e2e.file
-    python e2e_test.py tox-app:8080 e2e.file &
+    cat e2e | head -n $i | tail -n 25 > e2e.file$i
+    python e2e_test.py tox-app:8080 e2e.file$i &
 done
 sleep 2
