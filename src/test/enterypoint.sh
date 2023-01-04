90dce93320b0edfc41a91c3834aba20ea1a19fc5
Ubuntu <pashutdvir@gmail.com>
2023-01-04 18:36:15 +0000

    fix all

--- src/test/enterypoint.sh
+++ src/test/enterypoint.sh
@@ -9,7 +9,7 @@ cat e2e | head -n 300 | tail -n 50 > e2e.251-300
 cat e2e | head -n 350 | tail -n 50 > e2e.301-350
 cat e2e | head -n 399 | tail -n 50 > e2e.350-399
 
-
+sleep 1
 python e2e_test.py tox-app:8080 e2e.51-100   &
 python e2e_test.py tox-app:8080 e2e.101-150  &
 python e2e_test.py tox-app:8080 e2e.151-200  &
